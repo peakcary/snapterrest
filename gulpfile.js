50f14c6abaa79807bb1d28fc3ab47c8f989b1958
peakcary <peakcary@163.com>
2016-06-30 09:42:47 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+
+gulp.task('default',function(){
+  // console.log('i am peak');
+  return browserify('./source/app.js')
+         .transform(babelify)
+         .bundle()
+         .pipe(source('snapterest.js'))
+         .pipe(gulp.dest('./build/'));
+});
